63282872c61f6dc2fee9b1260c621b16e3bd942d
Tek N. Chaudhary <tekchau21@gmail.com>
2023-07-26 10:31:19 +0530

    new file:   backend/.gitignore 	new file:   backend/package-lock.json 	new file:   backend/package.json 	new file:   backend/server.js 	new file:   customsearch/.gitignore 	new file:   customsearch/package-lock.json 	new file:   customsearch/package.json 	new file:   customsearch/public/favicon.ico 	new file:   customsearch/public/index.html 	new file:   customsearch/public/logo192.png 	new file:   customsearch/public/logo512.png 	new file:   customsearch/public/manifest.json 	new file:   customsearch/public/robots.txt 	new file:   customsearch/src/App.css 	new file:   customsearch/src/App.js 	new file:   customsearch/src/App.test.js 	new file:   customsearch/src/index.css 	new file:   customsearch/src/index.js 	new file:   customsearch/src/logo.svg 	new file:   customsearch/src/reportWebVitals.js 	new file:   customsearch/src/setupTests.js

---
+++ backend/server.js
@@ -0,0 +1,81 @@
+
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+const scrapingbee = require('scrapingbee');
+const cheerio = require('cheerio');
+require('dotenv').config();
+
+app.use(cors());
+
+const CUSTOM_SEARCH_API_KEY = process.env.CUSTOM_SEARCH_API_KEY;
+const CUSTOM_SEARCH_ENGINE_ID = process.env.CUSTOM_SEARCH_ENGINE_ID;
+const SCRAPINGBEE_API_KEY = process.env.SCRAPINGBEE_API_KEY;
+
+app.get('/search', async (req, res) => {
+  const query = req.query.q;
+  const url = `https://www.googleapis.com/customsearch/v1`;
+  const params = {
+    key: CUSTOM_SEARCH_API_KEY,
+    cx: CUSTOM_SEARCH_ENGINE_ID,
+    q: query,
+    num: 5,
+  };
+
+  try {
+    const response = await axios.get(url, { params });
+    const searchResults = response.data.items.map((item) => item.link);
+    console.log(searchResults);
+    res.json(searchResults);
+  } catch (error) {
+    console.error('Error fetching search results:', error.message);
+    res.status(500).json({ error: 'Error fetching search results' });
+  }
+});
+
+app.get('/scrape', async (req, res) => {
+  const urls = req.query.urls.split(',');
+
+  try {
+    if (!urls.length) {
+      throw new Error('No URLs provided.');
+    }
+    const scrapingBeeClient = new scrapingbee.ScrapingBeeClient(SCRAPINGBEE_API_KEY);
+
+    const scrapePromises = urls.map((url) => scrapingBeeClient.get({ url }, { filter: { text: 'body' } }));
+
+    const scrapedResponses = await Promise.all(scrapePromises);
+
+    console.log(scrapedResponses);
+
+    const scrapedData = scrapedResponses.map((response) => {
+      const contentType = response.headers['content-type'];
+      if (!contentType || !contentType.includes('text/html')) {
+        throw new Error('Invalid response content-type');
+      }
+      const responseData = response.data.toString();
+      const cleanedData = responseData.replace(/<iframe\b[^>]*>(.*?)<\/iframe>/gi, '');
+      const $ = cheerio.load(cleanedData);
+      $('script, style').remove();
+      const bodyText = $('body').text();
+      const words = bodyText.trim().split(/\s+/).slice(0, 50);
+      const shortenedText = words.join(' ');
+    
+      return {
+        url: response.headers['spb-resolved-url'],
+        text:shortenedText,
+      };
+    });
+
+    res.json(scrapedData);
+  } catch (error) {
+    console.error('Error scraping URLs:', error.message);
+    res.status(500).json({ error: 'Error scraping URLs' });
+  }
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
---
+++ customsearch/src/App.js
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [query, setQuery] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [scrapedData, setScrapedData] = useState([]);
+
+  const handleQueryChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetchUrls();
+  };
+
+  const fetchUrls = () => {
+    axios
+      .get(`http://localhost:5000/search?q=${query}`)
+      .then((response) => {
+        setSearchResults(response.data);
+        scrapeData(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching URLs:', error.message);
+      });
+  };
+
+  const scrapeData = (urls) => {
+    const encodedUrls = urls.map((url) => encodeURIComponent(url));
+    axios
+      .get(`http://localhost:5000/scrape?urls=${encodedUrls.join(',')}`)
+      .then((response) => {
+        setScrapedData(response.data);
+        alert("Scraping Successful")
+      })
+      .catch((error) => {
+        alert("The Query couldn't be scraped");
+        console.error('Error scraping URLs:', error.message);
+      });
+  };
+
+  return (
+    <div>
+      <div className='header'>
+        <h1>Website Scrapper</h1>
+      </div>
+      <form className='formHandler' onSubmit={handleSubmit}>
+        <input type="text" value={query} placeholder='Enter your Query' onChange={handleQueryChange} />
+        <button type="submit">Search</button>
+      </form>
+      <div className='Scrapping'>
+        <p>Note: Please Wait a couple of seconds to complete the Scrapping</p>
+        <p>Possible failure of scraping</p>
+        <p>1. Youtube website</p>
+        <p>2. Instagram website</p>
+      </div>
+      {scrapedData.length > 0 && (
+        <div className='container'>
+          <h2>Scraped Text Content</h2>
+          {scrapedData.map((data, index) => (
+            <div key={index}>
+              <h3>URL: {data.url}</h3>
+              <p>{data.text}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
